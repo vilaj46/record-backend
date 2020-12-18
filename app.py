025cf85b72e9487cf92bfbd71a619cd3b2b994ad
Julian <vilaj46@gmail.com>
2020-12-18 16:27:06 -0500

    Uncommented the app.py

--- app.py
+++ app.py
@@ -25,52 +25,52 @@ def index():
     return 'Index Page'
 
 
-# @app.route('/upload', methods=['POST'])
-# def upload():
-#     # upload_data = upload_route(request.files['file'])
-#     # upload_data_keys = upload_data.keys()
+@app.route('/upload', methods=['POST'])
+def upload():
+    # upload_data = upload_route(request.files['file'])
+    # upload_data_keys = upload_data.keys()
 
-#     file1 = request.files['file']
-#     fileStream = file1.stream.read()
-#     doc = fitz.open(stream=fileStream, filetype='pdf')
-#     doc.save('%s/test.pdf' % (UPLOAD_FOLDER))
+    file1 = request.files['file']
+    fileStream = file1.stream.read()
+    doc = fitz.open(stream=fileStream, filetype='pdf')
+    doc.save('%s/test.pdf' % (UPLOAD_FOLDER))
 
-#     return send_file('./uploads/test.pdf')
-# # if len(upload_data_keys) == 0:
-# #     return {}
-# # else:
-# #     return upload_data
-
-
-# @app.route('/toc', methods=['GET', 'POST'])
-# def toc():
-#     if request.method == 'GET':
-#         toc_data = get_toc_route()
-#         return toc_data
-#     else:
-#         new_entry = post_toc_route(request)
-#         return new_entry
+    return send_file('./uploads/test.pdf')
+# if len(upload_data_keys) == 0:
+#     return {}
+# else:
+#     return upload_data
 
 
-# # We are on the PUT method.
-# # Change page number functionality
-# # Error checks fuctionality as well.
+@app.route('/toc', methods=['GET', 'POST'])
+def toc():
+    if request.method == 'GET':
+        toc_data = get_toc_route()
+        return toc_data
+    else:
+        new_entry = post_toc_route(request)
+        return new_entry
 
-# # Figure out what we want to return for both
-# # PUT and delete. Do we want to have a general entry errors for all of them
-# # or a page number errors for all of them. Incorporate the certification page
-# # number when calculating the page_number_in_pdf error.
 
-# @app.route('/toc/<id_number>', methods=['DELETE', 'PUT'])
-# def toc_id_number(id_number):
-#     # entries = FILE.entries
+# We are on the PUT method.
+# Change page number functionality
+# Error checks fuctionality as well.
 
-#     if request.method == 'DELETE':
-#         deleted_entry = delete_toc_entry_route(id_number, request)
-#         return deleted_entry
-#     else:  # PUT
-#         updated_entry = update_toc_entry_route(id_number, request)
-#         return updated_entry
+# Figure out what we want to return for both
+# PUT and delete. Do we want to have a general entry errors for all of them
+# or a page number errors for all of them. Incorporate the certification page
+# number when calculating the page_number_in_pdf error.
+
+@app.route('/toc/<id_number>', methods=['DELETE', 'PUT'])
+def toc_id_number(id_number):
+    # entries = FILE.entries
+
+    if request.method == 'DELETE':
+        deleted_entry = delete_toc_entry_route(id_number, request)
+        return deleted_entry
+    else:  # PUT
+        updated_entry = update_toc_entry_route(id_number, request)
+        return updated_entry
 
 
 # app.run(host='localhost', port=8080, debug=True)
