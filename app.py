b8e94e292aa391107ebee21558e8a8c6efe4a5f9
Julian <vilaj46@gmail.com>
2020-12-22 13:54:05 -0500

    Changed app.run launch

--- app.py
+++ app.py
@@ -86,6 +86,6 @@ def headers():
     return send_file('./tmp/test.pdf')
 
 
-app.run(host='localhost', port=8080, debug=True)
-# if __name__ == '__main__':
-#     app.run()
+# app.run(host='localhost', port=8080, debug=True)
+if __name__ == '__main__':
+    app.run()
